b1ba1ec6c8157e96c6fa1fd8e491c87278230df1
muonw <107559405+muonw@users.noreply.github.com>
2023-03-24 10:14:03 -0400

    demo home link fixed

--- docs/_app/immutable/chunks/stores.f82fec24.js
+++
@@ -1,1 +0,0 @@
-import"./index.5349f71a.js";import{s as e}from"./singletons.d8bd9a17.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
