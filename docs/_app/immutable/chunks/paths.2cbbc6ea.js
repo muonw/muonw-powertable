ce50210afc9aead43bb28a9493a8a9e3b100789a
muonw <107559405+muonw@users.noreply.github.com>
2023-07-29 16:30:50 -0400

    docs fixed

---
+++ docs/_app/immutable/chunks/paths.2cbbc6ea.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_bwu25z)==null?void 0:s.base)??"/muonw-powertable";var e;const t=((e=globalThis.__sveltekit_bwu25z)==null?void 0:e.assets)??a;export{t as a,a as b};
